3db178762354601376e0cea0865993645922072b
Yamawine <yamawine408@gmail.com>
2026-08-26 15:49:43 +0900

    Fix indentation in async_press method

--- custom_components/eew_alert_ha/button.py
+++ custom_components/eew_alert_ha/button.py
@@ -37,9 +37,9 @@ class EewAlertTestButton(ButtonEntity):
         self._attr_unique_id = f"{entry.entry_id}_test_button"
 
     async def async_press(self) -> None:
-		test_prefs = (
-		    [{"pref": f"{pref}県", "scale": 50} for pref in target_prefs]
-		)
+        test_prefs = (
+            [{"pref": f"{pref}県", "scale": 50} for pref in target_prefs]
+        )
 
         self._listener.state = EewState(
             label="5強",
